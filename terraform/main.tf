f9422b075edb1632c679fd6b95e15607e5ab4f31
pauloricassio <paulo_ricassio@hotmail.com>
2023-02-27 09:58:34 -0300

    Adicionando infra com Terraform e Ansible

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web-server" {
+  ami           = "ami-024614f01f42eeb66"
+  instance_type = "t2.micro"
+  key_name = "access_windows"
+  vpc_security_group_ids = [ "sg-08a0b00e075713d03" ]
+
+  user_data = <<-EOF
+    <powershell>
+      # Configuring WinRM
+      winrm quickconfig -q
+      winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
+      winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+      winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+      winrm set winrm/config/service/auth '@{Basic="true"}'
+
+      # Opening WinRM port in Windows Firewall
+      netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
+      netsh advfirewall firewall add rule name="WinRM-HTTPS" dir=in localport=5986 protocol=TCP action=allow
+    </powershell>
+  EOF
+ 
+  tags = {
+    Name = "Web-Server"
+}
+
+}
+
+output "public_ip" {
+  value = aws_instance.web-server.public_ip
+}
