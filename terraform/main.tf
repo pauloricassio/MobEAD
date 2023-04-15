20eb1178b134deebc8b2db76a0710adccce6e8ad
pauloricassio <paulo_ricassio@hotmail.com>
2023-04-14 22:03:55 -0300

    Alterando a chave ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "web-server" {
   ami           = "ami-024614f01f42eeb66"
   instance_type = "t2.micro"
-  key_name = "access_windows"
+  key_name = "/media/paulo/DATA/chaves_ssh/terraform"
   vpc_security_group_ids = [ "sg-08a0b00e075713d03" ]
 
   user_data = <<-EOF
@@ -22,7 +22,7 @@ resource "aws_instance" "web-server" {
       # Configuring WinRM
       winrm quickconfig -q
       winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
-      winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+      winrm set winrm/config '@{MaxTimeoutms="1800000"}'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
       winrm set winrm/config/service '@{AllowUnencrypted="true"}'
       winrm set winrm/config/service/auth '@{Basic="true"}'
 
